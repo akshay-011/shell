dd0299dd82037905570b28019fde4d99c7822177
akshay-011 <102289107+akshay-011@users.noreply.github.com>
2024-12-19 15:18:35 +0530

    changed prompt message & not found message

--- eggshell.js
+++ eggshell.js
@@ -1,10 +1,28 @@
-const promptMesssage = "akshay-kumar >";
+const NOT_FOUND = "esh: command not found: ";
+const promptMesssage = "akshaykumar@Akshays-MacBook-Pro shell %";
+
+const echo = function (args) {
+  console.log(args.join(" "));
+};
+
+function runCommand([command, ...args]) {
+  switch (command) {
+    case "echo":
+      return echo(args);
+    default:
+      return NOT_FOUND + command;
+  }
+}
+
 
 function eggshell() {
   while (true) {
     const commandString = prompt(promptMesssage);
-    console.log("you entered ", commandString);
+    const resultAfterRunning = runCommand(commandString.split(" "));
+
+    if (resultAfterRunning !== undefined) {
+      console.log(resultAfterRunning);
+    }
   }
 }
-
 eggshell();
